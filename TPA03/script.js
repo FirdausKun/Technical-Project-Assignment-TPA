81d6d86edc4791a22064adc14eb3e04e76f615a2
USERNAME <firdauskun00@gmail.com>
2022-10-13 22:22:00 +0800

    finish

--- TPA03/script.js
+++ TPA03/script.js
@@ -1,13 +1,16 @@
 // List Movie
-let containerMovies = document.querySelector("#list-movie")
-let getListMovies = async()=>{
-    let response = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=6498a2ab00dfbdea32609eec6bc9f04b&sort_by=popularity.desc")
-    let movies = await response.json()
+let containerMovies = document.querySelector('#list-movie');
+let getListMovies = async (query) => { 
+  let response = await fetch(
+    query ||
+      'https://api.themoviedb.org/3/discover/movie?api_key=6498a2ab00dfbdea32609eec6bc9f04b&sort_by=popularity.desc'
+  ); 
+  let movies = await response.json();
 
-    let movieData = movies.results
-    movieData.slice(0,18).forEach(item=> {
-        containerMovies.innerHTML +=
-        `<div class="col">
+  let movieData = movies.results;
+  containerMovies.innerHTML = ''; 
+  movieData.slice(0, 18).forEach((item) => {
+    containerMovies.innerHTML += `<div class="col">
         <div class="card m-5 border-info" style="width: 16rem;">
                 <img src="https://image.tmdb.org/t/p/w500/${item.poster_path}" class="card-img-top" alt="poster film ${item.title}">
                 <div class="card-body">
@@ -15,26 +18,25 @@ let getListMovies = async()=>{
                     <h5 class="card-title">${item.vote_average}</h5>
                     <p class="card-text">${item.release_date}</p>
                 </div>
-          </div>`
-    });
-}
-getListMovies()
+          </div>`;
+  });
+};
+getListMovies();
 
 // search Movie
+let form = document.getElementsByClassName('my-form')[0]; 
+let search = document.getElementById('search');
 
-let form =  document.getElementsByClassName("my-form")
-let search = document.getElementById("search")
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-
-form.addEventListener('submit', async (event) => {
-    event.preventDefault()
-
-    let searchTerm = search.value;
-    if(searchTerm) {
-        getListMovies("https://api.themoviedb.org/3//search/movie?api_key=6498a2ab00dfbdea32609eec6bc9f04b&query=${query}&page=1")
-    }else{
-        getListMovies("https://api.themoviedb.org/3/discover/movie?api_key=6498a2ab00dfbdea32609eec6bc9f04b&sort_by=popularity.desc")
-    }
-})
-console.log(form);
+  let searchTerm = search.value;
+  if (searchTerm) {
+    getListMovies(
+      `https://api.themoviedb.org/3/search/movie?api_key=6498a2ab00dfbdea32609eec6bc9f04b&query=${searchTerm}&page=1`
+    ); 
+  } else {
+    getListMovies(); 
+  }
+});
 console.log(form);
